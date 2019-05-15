13502055389c129586ab03884247b5ee1efea933
masanaoS2 <bimi0370@gmail.com>
2019-05-16 03:09:21 +0700

    update sanitize function

--- common/common.php
+++ common/common.php
@@ -1,11 +1,11 @@
 <?php
 
-function sanitize($before) {
-  foreach ($before as $key => $value) {
-    $after[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+function sanitize($post) {
+  foreach ($post as $key => $value) {
+    $sanitized_post[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
   }
 
-  return $after;
+  return $sanitized_post;
 }
 
 function connectDB() {
